984c4868fbcfaf8bfb82f59930cc7c1e13f18a21
Cabr3l <100071031+Cabr3l@users.noreply.github.com>
2022-02-21 20:51:11 +0000

    Update Configuration.py

--- Configuration.py
+++ Configuration.py
@@ -5,15 +5,23 @@ from tkinter.ttk import *
 from tkinter import ttk 
 from time import strftime
 
-
 class ConfigurationObject(object):
     "Klasse für die Sinus Spannung"
 
     def __init__(self):
-        self.projectTitle = 'Transient Analysis'
-        
-    def showConfiguration(self,panelwindowNetworkConfiguration):
-        #Create Frams  
+        pass
+ 
+    def showConfiguration(self,panelwindowNetworkConfiguration, impedance, amplitude, capacite, pulsation, y0, x1,x2):
+        #Create Frames  
         frameConfiguration=ttk.Frame(panelwindowNetworkConfiguration,width=400,height=300, relief=SUNKEN)  
         panelwindowNetworkConfiguration.add(frameConfiguration, weight=1)
         
+        frameConfiguration.update()
+        frameWidth = frameConfiguration.winfo_width()
+        label_impedance=ttk.Label(frameConfiguration, text="Impédance z\u2081 : ", width= int(frameWidth*3/80),anchor=W)
+        impe=ttk.Entry(frameConfiguration, textvariable=impedance, width=int(frameWidth*8/80), justify="left")
+        ohm= ttk.Label(frameConfiguration, text="[Ohms](\u03A9)",width=int(frameWidth*2/80), justify="right", anchor=W)
+        label_impedance.grid(row=1, column=0, pady=3, padx=2)
+        impe.grid(row = 1, column=1)
+        ohm.grid(row = 1, column = 2)
+                
