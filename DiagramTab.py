d5d3ba8a7435fbcc5c08fff821653e1cd90f14b6
Cabr3l <100071031+Cabr3l@users.noreply.github.com>
2022-02-21 23:22:38 +0000

    Update DiagramTab.py
    Ici je gère le pb d'image

--- DiagramTab.py
+++ DiagramTab.py
@@ -8,12 +8,15 @@ from time import strftime
 
 class DiagramTabObject(object):
     "Klasse für die Sinus Spannung"
-
     def __init__(self):
         self.projectTitle = 'Transient Analysis'
         
-    def showDiagram(self,panelwindow):
+    def showDiagram(self,panelwindow,img_0):
         #Create Frams  
+
         frameDiagram=ttk.Frame(panelwindow,width=500,height=400, relief=SUNKEN) 
         panelwindow.add(frameDiagram, weight=1)
+        cv = Canvas(frameDiagram,width = 500, height=400, background="ivory")
+        cv.create_image(0,0, anchor=NW, image=img_0)
+        cv.pack(padx=10, pady = 10)
         
