dc4f29ff36859911e2580ff412322e9323e1081d
695338035 <100071697+695338035@users.noreply.github.com>
2022-02-22 12:11:35 +0100

    Update DiagramTab.py

--- DiagramTab.py
+++ DiagramTab.py
@@ -5,6 +5,32 @@ from tkinter.ttk import *
 from tkinter import ttk 
 from time import strftime
 
+# ça c'est la puissante classse qui contient la methode me permettant de faire varier la taille de l'image avec la taille du frame dans lequel il se trouve
+class IMAGE(Frame):
+    def __init__(self, master, *pargs):
+        Frame.__init__(self, master, *pargs)
+
+        self.image = Image.open("image.png")
+        self.img_copy= self.image.copy()
+
+
+        self.background_image = ImageTk.PhotoImage(self.image)
+
+        self.background = Label(self, image=self.background_image)
+        self.background.pack(fill=BOTH, expand=YES)
+        self.background.bind('<Configure>', self._resize_image)
+        
+    #fvoici la puissante méthode en question
+    def _resize_image(self,event):
+
+        new_width = event.width
+        new_height = event.height
+
+        self.image = self.img_copy.resize((new_width, new_height))
+
+        self.background_image = ImageTk.PhotoImage(self.image)
+        self.background.configure(image =  self.background_image)
+
 
 class DiagramTabObject(object):
     "Klasse für die Sinus Spannung"
@@ -17,10 +43,7 @@ class DiagramTabObject(object):
 
         frameDiagram=ttk.Frame(panelwindow,width=500,height=400, relief=SUNKEN) 
         panelwindow.add(frameDiagram, weight=1)
-        cv = Canvas(frameDiagram,width = 500, height=400, background="ivory")
-        cv.create_image(0,0, anchor=NW, image=img_0)
-        cv.grid(row = 0, column=0,padx=10, pady = 10)
-        frameDiagram.grid_columnconfigure(0,weight=1)
-
+        cv = IMAGE(frameDiagram)
+        cv.pack(fill=BOTH,expand=True)
         self.frameDiagram = frameDiagram #This is done so I can access it elsewhere
         
