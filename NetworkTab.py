12f30a3bbbcf7cee7a41ab5d915766086a9c0c90
Cabrel TIOTSOP <100071031+Cabr3l@users.noreply.github.com>
2022-02-22 23:47:48 +0000

    Update NetworkTab.py

--- NetworkTab.py
+++ NetworkTab.py
@@ -7,33 +7,7 @@ from tkinter import ttk
 from time import strftime
 import time
 import os
-from PIL import Image, ImageTk
-
-#cette classe est analogue à la fonction IMAGE du fichier DiagramTab
-class IMAGE(Frame):
-    def __init__(self, master, *pargs):
-        Frame.__init__(self, master, *pargs)
-
-        self.image = Image.open("circuit.png")
-        self.img_copy= self.image.copy()
-
-
-        self.background_image = ImageTk.PhotoImage(self.image)
-
-        self.background = Label(self, image=self.background_image)
-        self.background.pack(fill=BOTH, expand=YES)
-        self.background.bind('<Configure>', self._resize_image)
-
-    def _resize_image(self,event):
-
-        new_width = event.width
-        new_height = event.height
-
-        self.image = self.img_copy.resize((new_width, new_height))
-
-        self.background_image = ImageTk.PhotoImage(self.image)
-        self.background.configure(image =  self.background_image)
-
+from resizableImage import IMAGE
 
 class NetworkTabObject(object):
     "Klasse für die Sinus Spannung"
@@ -47,7 +21,8 @@ class NetworkTabObject(object):
         frameNetwork=ttk.Frame(panelwindowNetworkConfiguration,width=100,height=80, relief=SUNKEN)
         panelwindowNetworkConfiguration.add(frameNetwork, weight=1)
         self.frameNetwork = frameNetwork
-        cv=IMAGE(frameNetwork)
+        
+        cv=IMAGE(frameNetwork,"circuit.png")
         cv.pack(fill=BOTH, expand=True)
         
     
