80dbf8474313756cc2458e01ae361cd4873d91f5
Cabrel TIOTSOP <100071031+Cabr3l@users.noreply.github.com>
2022-02-22 23:46:08 +0000

    Update DiagramTab.py

--- DiagramTab.py
+++ DiagramTab.py
@@ -5,32 +5,7 @@ from tkinter.ttk import *
 from tkinter import ttk 
 from time import strftime
 from PIL import Image, ImageTk
-
-# ça c'est la puissante classse qui contient la methode me permettant de faire varier la taille de l'image avec la taille du frame dans lequel il se trouve
-class IMAGE(Frame):
-    def __init__(self, master,nom):
-        Frame.__init__(self, master)
-
-        self.image = Image.open(f"{nom}")
-        self.img_copy= self.image.copy()
-
-        self.background_image = ImageTk.PhotoImage(self.image)
-
-        self.background = Label(self, image=self.background_image)
-        self.background.pack(fill=BOTH, expand=YES)
-        self.background.bind('<Configure>', self._resize_image)
-        
-    #fvoici la puissante méthode en question
-    def _resize_image(self,event):
-
-        new_width = event.width
-        new_height = event.height
-
-        self.image = self.img_copy.resize((new_width, new_height))
-
-        self.background_image = ImageTk.PhotoImage(self.image)
-        self.background.configure(image =  self.background_image)
-
+from resizableImage import IMAGE
 
 class DiagramTabObject(object):
     "Klasse für die Sinus Spannung"
@@ -38,7 +13,7 @@ class DiagramTabObject(object):
         self.projectTitle = 'Transient Analysis'
         self.frameDiagram = NONE
         
-    def showDiagram(self,panelwindow,img_0):
+    def showDiagram(self,panelwindow):
         #Create Frams  
 
         frameDiagram=ttk.Frame(panelwindow,width=500,height=400, relief=SUNKEN) 
