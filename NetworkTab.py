d46b30128ab20a38590f783cfc2a6b1a0adcbf47
695338035 <100071697+695338035@users.noreply.github.com>
2022-02-22 12:14:55 +0100

    Update NetworkTab.py

--- NetworkTab.py
+++ NetworkTab.py
@@ -8,6 +8,31 @@ from time import strftime
 import time
 import os
 
+#cette classe est analogue à la fonction IMAGE du fichier DiagramTab
+class IMAGE(Frame):
+    def __init__(self, master, *pargs):
+        Frame.__init__(self, master, *pargs)
+
+        self.image = Image.open("circuit.png")
+        self.img_copy= self.image.copy()
+
+
+        self.background_image = ImageTk.PhotoImage(self.image)
+
+        self.background = Label(self, image=self.background_image)
+        self.background.pack(fill=BOTH, expand=YES)
+        self.background.bind('<Configure>', self._resize_image)
+
+    def _resize_image(self,event):
+
+        new_width = event.width
+        new_height = event.height
+
+        self.image = self.img_copy.resize((new_width, new_height))
+
+        self.background_image = ImageTk.PhotoImage(self.image)
+        self.background.configure(image =  self.background_image)
+
 
 class NetworkTabObject(object):
     "Klasse für die Sinus Spannung"
@@ -21,6 +46,8 @@ class NetworkTabObject(object):
         frameNetwork=ttk.Frame(panelwindowNetworkConfiguration,width=100,height=100, relief=SUNKEN)
         panelwindowNetworkConfiguration.add(frameNetwork, weight=1)
         self.frameNetwork = frameNetwork
+        cv=IMAGE(frameNetwork)
+        cv.pack(fill=BOTH, expand=True)
         
     
 
